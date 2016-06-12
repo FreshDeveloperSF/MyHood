0968e3d3cb8ef14f82c286a4d5cac3bb14a59f16
Jason Leung <jasonleung@jasons-mbp-3.att.net>
2016-06-11 18:46:40 -0700

    Added second ViewController

---
+++ MyHood/AddPostVC.swift
@@ -0,0 +1,19 @@
+//
+//  AddPostVC.swift
+//  MyHood
+//
+//  Created by Jason Leung on 6/11/16.
+//  Copyright © 2016 Jason Leung. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
